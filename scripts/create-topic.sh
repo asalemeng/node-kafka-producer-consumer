9bd36d5b6d893e18c6ad57c7658beb6e2bb72bd8
SALEM ISKANDER <49528372+asalemeng@users.noreply.github.com>
2021-12-30 21:27:29 +0300

    Update create-topic.sh

--- scripts/create-topic.sh
+++ scripts/create-topic.sh
@@ -4,3 +4,13 @@ docker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
     --replication-factor 1 \
     --partitions 1 \
     --topic test
+    
+    
+    
+Newer versions(2.2 and above) of Kafka no longer require ZooKeeper connection
+docker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
+    --create \
+    --bootstrap-server localhost:9092 \
+    --replication-factor 1 \
+    --partitions 1 \
+    --topic tes
